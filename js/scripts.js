ceb25318abcfa8f29366211bd7c25a85b47dac2c
Sincere Villanueva III <svillanueva.git@gmail.com>
2022-10-14 22:39:08 -0700

    Write function to match both passwords If the passwords do not match, a custom error message displays.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
 
+const passMatch = document.getElementById("confirm-pwd");
+
+passMatch.addEventListener("input", (event) => {
+  const pw1 = document.getElementById("pwd");
+  if (passMatch.value != pw1.value) {
+    passMatch.setCustomValidity("Passwords do not match!");
+    passMatch.reportValidity();
+  } else {
+    passMatch.setCustomValidity("");
+  }
+});
